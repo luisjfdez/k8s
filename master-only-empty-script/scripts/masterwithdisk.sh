bd7037142299dc966e0ead1bafa2e68ff4eed85d
Luis Javier Fernandez Jaraba <luisj@microsoft.com>
2020-08-04 13:03:04 +0200

    remove nvidia-docker2 install in cpu mode

--- master-only-empty-script/scripts/masterwithdisk.sh
+++ master-only-empty-script/scripts/masterwithdisk.sh
@@ -150,14 +150,17 @@ disk_uuid_to_fstab="UUID=${disk_uuid}   /data  ext4    defaults,nofail   1  2)"
 echo $disk_uuid_to_fstab | sudo tee -a /etc/fstab
 
 
-# Add the package repositories
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
+if [ ! -z "$GPU_INSTALL" ]
+then
+  # Add the package repositories
+  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  sudo apt-get update
 
-# Install nvidia-docker2 and reload the Docker daemon configuration
-sudo apt-get install -y nvidia-docker2
+  # Install nvidia-docker2 and reload the Docker daemon configuration
+  sudo apt-get install -y nvidia-docker2
+fi
 
 if [ ! -z "$GPU_INSTALL" ]
 then
